3cc2977afae9b99627064772bb0d4d0a683ebb94
Mohammad Honarvar <mohammad.honarvar@partsoftware.com>
2021-05-23 23:50:09 +0430

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/style-prop-object */
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <p>Hello REACT 😎</p>
+  );
+};
+
+export default App;
